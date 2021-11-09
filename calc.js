d91b9217c692c6acb0d6908ee6229b039a9ec6fa
Daniel Zolman <zolman.daniel@gmail.com>
2021-11-09 16:07:57 -0700

    Added Power funtion. Allowing you to square, cube, or raise to the power of however many. Also changed the sequence you enter equations, no longer is it entered as mathSymbol then num1 and num2, it is num1 mathSymbol num2.

--- calc.js
+++ calc.js
@@ -5,13 +5,11 @@ const reader = readline.createInterface({
     output: process.stdout,
   });
   
-  
-  
   reader.question("What would you like to calculate?", function(input){
       tokens = input.split(' ');
       
-      mathSymbol = tokens[0];
-      num1 = Number(tokens[1]);
+      mathSymbol = tokens[1];
+      num1 = Number(tokens[0]);
       num2 = Number(tokens[2]);
 
       if (mathSymbol === "+"){
@@ -33,8 +31,12 @@ const reader = readline.createInterface({
     if (mathSymbol === "sqrt"){
         console.log(Math. sqrt(num1));
     }
+    
+    if (mathSymbol === "^"){
+        console.log(Math.pow(num1, num2))
+    }
 
       // This line closes the connection to the command line interface.
       reader.close()
-  
+
   });
