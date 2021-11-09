bf6fd8e84b2335c23b8609e0fea77ce84655e89b
Daniel Zolman <zolman.daniel@gmail.com>
2021-11-09 14:47:08 -0700

    Added and tested the following; Addition, Subtraction, Multiplication, Division, and Square Root. These are all working properly.

--- calc.js
+++ calc.js
@@ -1,3 +1,5 @@
+const readline = require('readline')
+
 const reader = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -11,8 +13,27 @@ const reader = readline.createInterface({
       mathSymbol = tokens[0];
       num1 = Number(tokens[1]);
       num2 = Number(tokens[2]);
-  
-  
+
+      if (mathSymbol === "+"){
+          console.log(num1 + num2);
+      }
+
+      if (mathSymbol === "-"){
+        console.log(num1 - num2);
+    }
+
+      if (mathSymbol === "*"){
+        console.log(num1 * num2);
+    }
+
+      if (mathSymbol === "/"){
+        console.log(num1 / num2);
+    }
+
+    if (mathSymbol === "sqrt"){
+        console.log(Math. sqrt(num1));
+    }
+
       // This line closes the connection to the command line interface.
       reader.close()
   
